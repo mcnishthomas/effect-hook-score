300a12ef76f673be22666fd5147372f10cf6d5df
Thomas McNish <mcnishthomas@gmail.com>
2022-05-21 10:35:37 -0400

    converted useEffect method to fetch dog images from API

--- src/App.js
+++ src/App.js
@@ -2,28 +2,18 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [score, setScore] = useState(0);
-  const [message] = useState('Welcome!');
+  const [data, setData] = useState('');
 
   useEffect(() => {
-    document.title = `${message}. Your score is ${score}`;
-  }, [message, score]);
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(data => setData(data.message))
+      .catch(err => console.log('Oh noes!', err))
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>{message}</h1>
-        <h2>{score}</h2>
-        <button onClick={() => setScore(prevScore => prevScore + 1)}>
-          Increase score
-        </button>
-        <button onClick={() => setScore(prevScore => prevScore - 1)}>
-          Decrease score
-        </button>
-        <button onClick={() => setScore(0)}>
-          Reset score
-        </button>
-      </header>
+      <img src={data} alt="A random dog breed" />
     </div>
   );
 }
